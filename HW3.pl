22e87ce9c1da299021c67ab0e65f7588b3dff196
Adam Wilford <canadianmoose8957@gmail.com>
2013-11-05 17:32:13 -0500

    Have time and get time from the flight() predicate.

--- HW3.pl
+++ HW3.pl
@@ -5,17 +5,24 @@
 %connect(cityD, cityE).
 
 %define the flights, a simple line of flights for now
+%flight(start city, end city, start time, flight duration)
 flight(cityA, cityB, 0, 1).
+flight(cityA,cityD,0,1).
 flight(cityB, cityC, 1, 2).
 flight(cityC, cityD, 2, 3).
 flight(cityD, cityE, 3, 4).
 flight(cityE, cityA, 4, 5).
 
-fly(From, To, Start, End) :- fly(From,To,Start,End,[]).
-fly(From,To,_,_,_) :- flight(From,To,_,_).
+timeCheck(Start,End,Time) :-
+	Time = End - Start,
+	Time =< End.
 
-fly(From, To, Start, End, Visited) :-
-    flight(From, Through,_,_),
+fly(From, To, Start, End) :- fly(From,To,Start,End,[], Time).
+fly(From,To,_,_,_,_) :- flight(From,To,_,_).
+
+fly(From, To, Start, End, Visited,Time) :-
+    flight(From, Through,FlightStart,FlightDuration),
     \+ memberchk(Through,Visited),
-    fly(Through,To,Start,End,[Through|Visited]),
+	timeCheck(FlightStart,FlightDuration,Time),
+    fly(Through,To,Start,End,[Through|Visited],Time),
     From \= To.
