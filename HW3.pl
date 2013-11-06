0eee1e607f96a9d566cb8e0ccc2254a27ac5266d
Adam Wilford <canadianmoose8957@gmail.com>
2013-11-06 11:11:54 -0500

    Time check seems to work, but need to add list for last duration.

--- HW3.pl
+++ HW3.pl
@@ -1,30 +1,23 @@
-%define the cities, a simple line of cities for now
-%connect(cityA, cityB).
-%connect(cityB, cityC).
-%connect(cityC, cityD).
-%connect(cityD, cityE).
-
 %define the flights, a simple line of flights for now
 %flight(start city, end city, start time, flight duration)
 flight(cityA, cityB, 0, 1).
 flight(cityA,cityD,0,1).
 flight(cityB, cityC, 1, 2).
 flight(cityC, cityD, 2, 3).
-flight(cityD, cityE, 3, 4).
+flight(cityD, cityE, 3, 7).
 flight(cityE, cityA, 4, 5).
 
-timeCheck(Start,End,Time) :-
-	Duration is End - Start,
-	TempTime is Time + Duration,
-	TempTime =< End,
-	Time is TempTime.
+timeCheck(Start,Duration,End,Time) :-
+	Start + Duration =< End.
 
-fly(From, To, Start, End) :- fly(From,To,Start,End,[], Time).
-fly(From,To,_,_,_,_) :- flight(From,To,_,_).
+fly(From, To, Start, End) :- fly(From,To,Start,End,[],[], Time).
+fly(From,To,_,End,_,Time) :-
+	flight(From,To,FlightStart,FlightDuration),
+	timeCheck(FlightStart,FlightDuration, End, Time).
 
 fly(From, To, Start, End, Visited,Time) :-
     flight(From, Through,FlightStart,FlightDuration),
     \+ memberchk(Through,Visited),
-	timeCheck(FlightStart,FlightDuration,Time),
+	timeCheck(FlightStart,FlightDuration,End,Time),
     fly(Through,To,Start,End,[Through|Visited],Time),
     From \= To.
