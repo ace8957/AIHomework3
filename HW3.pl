47a49f4f2d800837e2a320b6aa28d90aeb8a0b9f
Adam Wilford <canadianmoose8957@gmail.com>
2013-11-06 20:57:01 -0500

    wip

--- HW3.pl
+++ HW3.pl
@@ -1,24 +1,25 @@
 %define the flights, a simple line of flights for now
 %flight(start city, end city, start time, flight duration)
 flight(cityA, cityB, 0, 1).
-flight(cityA,cityD,0,1).
-flight(cityB, cityC, 1, 2).
-flight(cityC, cityD, 2, 3).
-flight(cityD, cityE, 3, 2).
-flight(cityE, cityA, 4, 5).
+%flight(cityA,cityD,0,1).
+flight(cityB, cityC, 2, 2).
+flight(cityC, cityD, 4, 3).
+flight(cityD, cityE, 7, 20).
+flight(cityE, cityA, 9, 5).
 
-timeCheck(Start,Duration,End,LastDuration) :-
-	nth0(0,LastDuration,LD),
-	LD + Duration =< End.
+timeCheck(Start,Duration,End,LastEnd) :-
+	nth0(0,LastEnd,LE),
+	LE + Duration =< End,
+	Start >= LE.
 
 fly(From, To, Start, End) :- fly(From,To,Start,End,[],[Start]).
-fly(From,To,_,End,_,LastDuration) :-
+fly(From,To,_,End,_,LastEnd) :-
 	flight(From,To,FlightStart,FlightDuration),
-	timeCheck(FlightStart,FlightDuration, End,LastDuration).
+	timeCheck(FlightStart,FlightDuration, End,LastEnd).
 
-fly(From, To, Start, End, Visited,LastDuration) :-
+fly(From, To, Start, End, Visited,LastEnd) :-
     flight(From, Through,FlightStart,FlightDuration),
     \+ memberchk(Through,Visited),
-	timeCheck(FlightStart,FlightDuration,End,LastDuration),
-    fly(Through,To,Start,End,[Through|Visited],[FlightStart+FlightDuration|LastDuration]),
+	timeCheck(FlightStart,FlightDuration,End,LastEnd),
+    fly(Through,To,Start,End,[Through|Visited],[FlightStart+FlightDuration|LastEnd]),
     From \= To.
